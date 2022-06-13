d3b1b8ae7a80ea244cf7f196c6ad8f21ddb7901e
Muddu Krishna Kommineni <95850851+muddu-engineer@users.noreply.github.com>
2022-06-13 19:16:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
 
-streamlit.title("Welcome")
-streamlit.header("Muddu")
-streamlit.text("happy learning")
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
